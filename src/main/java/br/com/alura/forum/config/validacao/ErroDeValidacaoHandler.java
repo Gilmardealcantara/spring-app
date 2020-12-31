21a00a5df25a4dd061c45345438fa536c89997c7
Gilmar <gilmardealcantara@gmail.com>
2020-12-31 02:23:29 -0300

    Add ErroDeValidacaoHandler as RestControllerAdvice

---
+++ src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
@@ -0,0 +1,33 @@
+package br.com.alura.forum.config.validacao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ErroDeValidacaoHandler {
+    @Autowired
+    private MessageSource messageSource;
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class )
+    public List<ErroDeFormularioDto> handler(MethodArgumentNotValidException exception) {
+        return exception
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(e -> new ErroDeFormularioDto(
+                        e.getField(),
+                        messageSource.getMessage(e, LocaleContextHolder.getLocale())
+                )).collect(Collectors.toList());
+    }
+}
