a1ed0d614dba36ba63190db50c67180f477853f4
Gilmar <gilmardealcantara@gmail.com>
2020-12-31 14:55:51 -0300

    Add order

--- src/main/java/br/com/alura/forum/controllers/TopicosController.java
+++ src/main/java/br/com/alura/forum/controllers/TopicosController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -30,8 +31,8 @@ public class TopicosController {
     private CursonRepository cursonRepository;
 
     @GetMapping
-    public Page<TopicoDto> lista(@RequestParam(required = false) String courseName, @RequestParam int page, @RequestParam int qtd) {
-        Pageable pageable = PageRequest.of(page, qtd);
+    public Page<TopicoDto> lista(@RequestParam(required = false) String courseName, @RequestParam int page, @RequestParam int qtd, @RequestParam String order) {
+        Pageable pageable = PageRequest.of(page, qtd, Sort.Direction.ASC, order);
 
         if (courseName == null) {
             Page<Topico> pages = topicoRepository.findAll(pageable);
